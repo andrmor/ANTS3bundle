640dabfc762cd3b207a6cdc05d576af8c3fc9fa7
Andr <andreimorozov@hotmail.com>
2021-10-23 12:36:32 +0100

    ++

--- ants3bundle/src/ants3/tools/agraphbuilder.h
+++ ants3bundle/src/ants3/tools/agraphbuilder.h
@@ -15,30 +15,30 @@ class QString;
 
 namespace AGraphBuilder
 {
-    static TGraph * graph(const QVector<double>& x, const QVector<double>& y);
+    TGraph * graph(const QVector<double>& x, const QVector<double>& y);
 
-    static TGraph * graph(const std::vector<float>& x, const std::vector<float>& y);
+    TGraph * graph(const std::vector<float>& x, const std::vector<float>& y);
 
-    static TGraph * graph(const QVector<double>& x, const QVector<double>& y,
+    TGraph * graph(const QVector<double>& x, const QVector<double>& y,
                           const char *Title, const char *XTitle, const char *YTitle,
                           short MarkerColor=2, int MarkerStyle=20, int MarkerSize=1,
                           short LineColor=2,   int LineStyle=1,    int LineWidth=2);
-    static TGraph * graph(const QVector<double>& x, const QVector<double>& y,
+    TGraph * graph(const QVector<double>& x, const QVector<double>& y,
                           const QString & Title, const QString & XTitle, const QString & YTitle,
                           short MarkerColor=2, int MarkerStyle=20, int MarkerSize=1,
                           short LineColor=2,   int LineStyle=1,    int LineWidth=2);
-    static TGraph * graph(const std::vector<float>& x, const std::vector<float>& y,
+    TGraph * graph(const std::vector<float>& x, const std::vector<float>& y,
                           const char *Title, const char *XTitle, const char *YTitle,
                           short MarkerColor=2, int MarkerStyle=20, int MarkerSize=1,
                           short LineColor=2,   int LineStyle=1,    int LineWidth=2);
 
-    static void configure(TGraph * graph, const QString & GraphTitle,
+    void configure(TGraph * graph, const QString & GraphTitle,
                           const QString & XTitle, const QString & YTitle,
                           int MarkerColor=2, int MarkerStyle=20, int MarkerSize=1,
                           int LineColor=2,   int LineStyle=1,    int LineWidth=2);
 
-    static TGraph2D * graph(const QVector<double>& x, const QVector<double>& y, const QVector<double>& z);
-    static TGraph2D * graph(const QVector<double>& x, const QVector<double>& y, const QVector<double>& z,
+    TGraph2D * graph(const QVector<double>& x, const QVector<double>& y, const QVector<double>& z);
+    TGraph2D * graph(const QVector<double>& x, const QVector<double>& y, const QVector<double>& z,
                             const char *Title, const char *XTitle, const char *YTitle, const char *ZTitle,
                             short MarkerColor=2, int MarkerStyle=20, int MarkerSize=1,
                             short LineColor=2,   int LineStyle=1,    int LineWidth=2);
