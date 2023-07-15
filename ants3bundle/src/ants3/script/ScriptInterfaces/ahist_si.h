8eeb59c5fb883b1355000643d56d51acab145a23
Andrey Morozov <andreimorozov@hotmail.com>
2023-07-15 10:58:04 +0100

    add  th2D with variable bin sizes

--- ants3bundle/src/ants3/script/ScriptInterfaces/ahist_si.h
+++ ants3bundle/src/ants3/script/ScriptInterfaces/ahist_si.h
@@ -28,6 +28,7 @@ public slots:
     void           new1D(QString histName, QVariantList binStartArray);
     void           new2D(QString histName, int binsX, double startX, double stopX,
                                            int binsY, double startY, double stopY);
+    void           new2D(QString histName, QVariantList xBinStartArray, QVariantList yBinStartArray);
     void           new3D(QString histName, int binsX, double startX, double stopX,
                                            int binsY, double startY, double stopY,
                                            int binsZ, double startZ, double stopZ);
